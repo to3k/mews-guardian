067d8d14abadd039a3867a88c29a667b602b1253
to3k <2579689+to3k@users.noreply.github.com>
2023-01-24 10:11:07 +0100

    Added #MEWS to hashtags list

--- theguardian.php
+++ theguardian.php
@@ -57,6 +57,7 @@
                         // PUSH REFORMATTED CATEGORY INTO ARRAY WITH HASHTAGS
                         $hashtag[] = $category;
                     }
+                    $hashtag[] = "#MEWS";
                     // GATHER ALL HASHTAGS TOGETHER INTO ONE STRING WITH SPACE AS SEPARATOR
                     $hashtags = implode(" ", $hashtag);
 
